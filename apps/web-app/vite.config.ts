133778537bab618aabfd7f86411caaa8abac28b1
Samuel Maddox <18106785+SamuelMaddox@users.noreply.github.com>
2026-03-12 16:58:17 -0400

    Added react router

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -6,7 +6,14 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [
+          [
+            'babel-plugin-react-compiler',
+            {
+              panicThreshold: 'none',
+            },
+          ],
+        ],
       },
     }),
   ],
